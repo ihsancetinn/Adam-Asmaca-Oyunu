4f73d36d849c528bd53cdedd811c9f6a35dd2972
ihsancetinn <ihsancetin92@gmail.com>
2022-07-07 12:55:42 +0300

    doğru/yanlış kelime işlemleri

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 const word_el = document.getElementById('word');
 const popup = document.getElementById('popup-container');
+const message_el = document.getElementById('success-message');
 
-const correctLetters = ['j','a','v','s','t','i','c',];
+
+
+
+
+const correctLetters = ['j','a','v','s','t','e','p','y'];
 const wrongLetters = [];
+const selectedWord = getRandomWord();
 
 function getRandomWord() {
     const words = ['javascript', 'python', 'java', 'django', 'reactjs', 'angularjs', 'vuejs', 'typescript', 'nodejs', 'nextjs'];
@@ -11,10 +17,8 @@ function getRandomWord() {
 };
 
 
-
-
 function displayWord() {
-    const selectedWord = getRandomWord();
+    
 
 
     word_el.innerHTML = `
@@ -25,11 +29,30 @@ function displayWord() {
 const w = word_el.innerText.replace(/\n/g, '');
 if( w === selectedWord){
     popup.style.display = 'flex';
-
-
+    message_el.innerText = 'Tebrikler Kazandınız.'
 }
 
-
-
 };
+window.addEventListener('keydown', function(e){
+
+    if(e.keyCode >= 65 && e.keyCode <=90) {
+   const letter = e.key;
+   if(selectedWord.includes(letter)) {
+    if(!correctLetters.includes(letter)) {
+        correctLetters.push(letter);
+        displayWord();
+    } else {
+        alert("Bu Harf Zaten Eklidir.")
+
+    }
+   } else { if(!wrongLetters.includes(letter))  {
+    wrongLetters.push(letter);
+    //Hatalı Harfleri Güncller
+    console.log('Hatalı Harfleri Günceller')
+   }
+
+   }
+}
+})
+
 displayWord();
