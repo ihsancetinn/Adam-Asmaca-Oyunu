d8c96be55beb1da27932ca1f4caf2426aa60abe3
ihsancetinn <ihsancetin92@gmail.com>
2022-07-07 11:46:07 +0300

    Alert Message

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 const word_el = document.getElementById('word');
+const popup = document.getElementById('popup-container');
+
+const correctLetters = ['j','a','v','s','t','i','c',];
+const wrongLetters = [];
 
 function getRandomWord() {
     const words = ['javascript', 'python', 'java', 'django', 'reactjs', 'angularjs', 'vuejs', 'typescript', 'nodejs', 'nextjs'];
     return words[Math.floor(Math.random() * words.length)];
 
-}
+};
+
+
+
 
 function displayWord() {
     const selectedWord = getRandomWord();
@@ -12,7 +19,17 @@ function displayWord() {
 
     word_el.innerHTML = `
      ${selectedWord.split('').map(letter => `
-     <div class="letter">${letter}</div>
+     <div class="letter">${correctLetters.includes(letter) ? letter : ''}</div>
      `).join('')}`;
+
+const w = word_el.innerText.replace(/\n/g, '');
+if( w === selectedWord){
+    popup.style.display = 'flex';
+
+
+}
+
+
+
 };
 displayWord();
